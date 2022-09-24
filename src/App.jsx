11879557b84abf1245b04202d8d241ab85f3f920
Victor Lucas A. de Oliveira <59880314+victorlucass@users.noreply.github.com>
2022-09-23 21:47:33 -0400

    finish sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Header } from "./components/Header";
 import "./global.css";
 import styles from "./app.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
-        <aside>sidebar</aside>
-        <main></main>
+        <Sidebar />
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam culpa
+          cupiditate suscipit exercitationem expedita, itaque quibusdam animi
+          sapiente veniam facere nobis dignissimos tempora totam repudiandae
+          placeat ab molestiae rem natus.
+        </main>
       </div>
     </>
   );
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1659535836241-9901856696b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="http://github.com/victorlucass.png"
+        />
+        <strong>Victor Lucas</strong>
+        <span>Dev. Front-End</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
